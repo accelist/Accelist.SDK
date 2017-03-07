1ea09f5ef1d0b04cee87e5ea0f423a723c330455
Ryan Elian <ryan.elian.cs@gmail.com>
2017-03-08 02:19:28 +0700

    Restructured project. Adds Accelist HTTP sink for Serilog.

---
+++ src/HttpUtil/AccelistLoggerConfigurationExtension.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using Serilog.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpUtil
+{
+    /// <summary>
+    /// Extension method for adding Serilog sink for Accelist Logger.
+    /// </summary>
+    public static class AccelistLoggerConfigurationExtension
+    {
+        /// <summary>
+        /// Adds a sink that sends log via HTTP to a remote logging server, with provided App ID in the request header.
+        /// </summary>
+        /// <param name="writeTo"></param>
+        /// <param name="appId"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static LoggerConfiguration Accelist(this LoggerSinkConfiguration writeTo, string appId, string url = "https://log.accelist.com")
+        {
+            var client = new AccelistLoggerClient(appId);
+            return writeTo.Http(url, new Serilog.Sinks.Http.Options(), httpClient: client);
+        }
+    }
+}
