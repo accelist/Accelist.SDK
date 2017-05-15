870a874a085f8346daf5bd6b67bf380c97302b99
Ryan Elian <ryan.elian.cs@gmail.com>
2017-05-15 15:45:41 +0700

    Un-log the command text for fragmentation scan method. Bug fix: log parameter wrongly ordered.

--- Accelist.SDK.SQL/Defragment/DbConnectionDefragmentExtensions.cs
+++ Accelist.SDK.SQL/Defragment/DbConnectionDefragmentExtensions.cs
@@ -25,7 +25,7 @@ namespace Accelist.SDK.SQL.Defragment
             var stopwatch = Stopwatch.StartNew();
 
             // First do a full-database index scan
-            var commandText = @"SELECT DISTINCT
+            var scans = await db.QueryAsync<IndexScan>(@"SELECT DISTINCT
     s.name as [Schema],
     t.name as [Table],
     ind.name as [Index],
@@ -39,16 +39,15 @@ JOIN sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic
 JOIN sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id 
 JOIN sys.tables t ON ind.object_id = t.object_id 
 JOIN sys.schemas s on s.schema_id = t.schema_id
-WHERE t.is_ms_shipped = 0 AND NOT (t.name = 'sysdiagrams')";
-            var scans = await db.QueryAsync<IndexScan>(commandText);
+WHERE t.is_ms_shipped = 0 AND NOT (t.name = 'sysdiagrams')");
 
             /*
             AND indexstats.page_count > 1000
             AND indexstats.avg_fragmentation_in_percent > 10
             */
 
-            Log.Information("Scanning SQL Server database {DatabaseName} index fragmentations: ({ElapsedMilliseconds} ms) {CommandText} {Scans}",
-                db.Database, commandText, scans, stopwatch.ElapsedMilliseconds);
+            Log.Information("Scanning SQL Server database {DatabaseName} index fragmentations: ({ElapsedMilliseconds} ms) {Scans}",
+                db.Database, stopwatch.ElapsedMilliseconds, scans);
             return scans;
         }
 
