db0f6a974dcbcdab40072da801132ad33b4d947b
Ryan Elian <ryan.elian.cs@gmail.com>
2017-06-29 01:58:37 +0700

    Major package upgrade. (Serilog.Sinks.Http)

--- Accelist.SDK.Logging/AccelistSerilogExtensions.cs
+++ Accelist.SDK.Logging/AccelistSerilogExtensions.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Accelist.SDK.Logging;
 using Serilog.Configuration;
 using Serilog.Events;
+using Serilog.Sinks.Http.TextFormatters;
 
 namespace Serilog
 {
@@ -25,15 +26,13 @@ namespace Serilog
         public static LoggerConfiguration Accelist(this LoggerSinkConfiguration sinkConfiguration, Guid apiKey, string server = null, LogEventLevel minimumSeverity = LogEventLevel.Information)
         {
             var dom = server?.TrimEnd('/') ?? "https://log.accelist.com";
-
-            var option = new Serilog.Sinks.Http.Options
-            {
-                FormattingType = Serilog.Sinks.Http.FormattingType.Normal,
-                Period = new TimeSpan(0, 0, 15)
-            };
-
-            var client = new AccelistLoggingClient(apiKey);
-            return sinkConfiguration.Http(dom + "/api/v1/log", option, minimumSeverity, client);
+            
+            return sinkConfiguration.Http(dom + "/api/v1/log", 
+                period: new TimeSpan(0, 0, 15),
+                textFormatter: new NormalTextFormatter(),
+                restrictedToMinimumLevel: minimumSeverity,
+                httpClient: new AccelistLoggingClient(apiKey)
+                );
         }
     }
 }
